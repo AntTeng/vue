63c3d0b203b69f83f7deae896a38d1b80c81695b
AntTeng <antteng@163.com>
2017-12-18 17:17:15 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <div class="dell" @click="handleClick">{{message}}</div>
-  </div>
+    <div id="app">
+        <div class="dell" @click="handleClick">{{message}}</div>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'app',
-    data () {
-      return {
-        message: 'hello world'
-      }
-    },
-    methods: {
-      handleClick () {
-        this.message = 'bye world'
-      }
+    export default {
+        name: 'app',
+        data () {
+            return {
+                message: 'hello world'
+            }
+        },
+        methods: {
+            handleClick () {
+                this.message = 'bye world'
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
-  .dell {
-    color: red;
-  }
+    .dell {
+        color: red;
+    }
 </style>
